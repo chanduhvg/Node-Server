6d7233b78130534f0bef6d9db31accccdd1e0fe3
chanduhvg <chanduhvg@gmail.com>
2020-03-25 00:25:29 -0500

    asdfghj

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3001;
+const hostname = process.env.hostname || '0.0.0.0';
+const port = process.env.PORT || 1408
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -10,5 +10,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running on ${port}/`);
 });
